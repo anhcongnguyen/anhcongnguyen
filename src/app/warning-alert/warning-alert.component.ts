3f536d67f5cdde2cea98c4dfacae3a31239874f1
cong anh <nguyenanhyoung@gmail.com>
2020-11-22 20:58:37 +0700

    Assignment 1: Practicing Components

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: `<p>This is warning alert !</p>`,
+  styles: [`p {
+    padding: 20px;
+    background-color: mistyrose;
+    border: 1px solid red;
+  }`]
+  // templateUrl: './warning-alert.component.html',
+  // styleUrls: ['./warning-alert.component.css']
+})
+export class WarningAlertComponent {
+
+
+}
